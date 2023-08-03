a40f3ef295418cc8b974eb7b00c39f17b3378ee7
Edvvxrd <edvvxrd@gmail.com>
2023-08-03 08:37:29 -0400

    All task Complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,91 +41,126 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
     //Write your code here
-    return res.status(200).send(books);
+    const getBooks = new Promise((resolve, reject) => {
+        try {
+            resolve(res.status(200).send(books));
+        } catch (error) {
+            reject(res.status(500).json({ mesasge: "Some Error occured in the server" }));
+        }
+    });
+    getBooks.then(() => console.log('Task 10: Resolved'));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     //Write your code here
-    const isbn = req.params.isbn;
-    let book = null;
+    const getBooks = new Promise((resolve, reject) => {
+        try {
+            const isbn = req.params.isbn;
+            let book = null;
 
-    Object.keys(books).forEach(key => {
-        if (key == isbn) {
-            book = books[key];
+            Object.keys(books).forEach(key => {
+                if (key == isbn) {
+                    book = books[key];
+                }
+            });
+
+            if (book != null) {
+                return res.status(200).json(book);
+            } else {
+                return res.status(404).json({ message: "No item found!" });
+            }
+        } catch (error) {
+            reject(res.status(500).json({ mesasge: "Some Error occured in the server" }));
         }
     });
-
-    if (book != null) {
-        return res.status(200).json(book);
-    } else {
-        return res.status(404).json({ message: "No item found!" });
-    }
+    getBooks.then(() => console.log('Task 11: Resolved'));
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     //Write your code here
-    const author = req.params.author;
-    let works = { "booksbyauthor": [] };
+    const getBooks = new Promise((resolve, reject) => {
+        try {
+            const author = req.params.author;
+            let works = { "booksbyauthor": [] };
 
-    Object.keys(books).forEach(key => {
-        if (books[key].author == author) {
-            works.booksbyauthor.push({
-                "isbn": key,
-                "title": books[key].title,
-                "reviews": books[key].reviews
+            Object.keys(books).forEach(key => {
+                if (books[key].author == author) {
+                    works.booksbyauthor.push({
+                        "isbn": key,
+                        "title": books[key].title,
+                        "reviews": books[key].reviews
+                    });
+                }
             });
+
+            if (works.booksbyauthor.length > 0) {
+                return res.status(200).json(works);
+            } else {
+                return res.status(404).json({ message: "No item found!" });
+            }
+        } catch (error) {
+            reject(res.status(500).json({ mesasge: "Some Error occured in the server" }));
         }
     });
-
-    if (works.booksbyauthor.length > 0) {
-        return res.status(200).json(works);
-    } else {
-        return res.status(404).json({ message: "No item found!" });
-    }
+    getBooks.then(() => console.log('Task 12: Resolved'));
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     //Write your code here
-    const title = req.params.title;
-    let works = { "booksbytitle": [] };
+    const getBooks = new Promise((resolve, reject) => {
+        try {
+            const title = req.params.title;
+            let works = { "booksbytitle": [] };
 
-    Object.keys(books).forEach(key => {
-        if (books[key].title == title) {
-            works.booksbytitle.push({
-                "isbn": key,
-                "author": books[key].author,
-                "reviews": books[key].reviews
+            Object.keys(books).forEach(key => {
+                if (books[key].title == title) {
+                    works.booksbytitle.push({
+                        "isbn": key,
+                        "author": books[key].author,
+                        "reviews": books[key].reviews
+                    });
+                }
             });
+
+            if (works.booksbytitle.length > 0) {
+                return res.status(200).json(works);
+            } else {
+                return res.status(404).json({ message: "No item found!" });
+            }
+        } catch (error) {
+            reject(res.status(500).json({ mesasge: "Some Error occured in the server" }));
         }
     });
-
-    if (works.booksbytitle.length > 0) {
-        return res.status(200).json(works);
-    } else {
-        return res.status(404).json({ message: "No item found!" });
-    }
+    getBooks.then(() => console.log('Task 13: Resolved'));
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
     //Write your code here
-    const isbn = req.params.isbn;
-    let book = null;
+    const getBooks = new Promise((resolve, reject) => {
+        try {
+            const isbn = req.params.isbn;
+            let book = null;
 
-    Object.keys(books).forEach(key => {
-        if (key == isbn) {
-            book = books[key].reviews;
+            Object.keys(books).forEach(key => {
+                if (key == isbn) {
+                    book = books[key].reviews;
+                }
+            });
+
+            if (book != null) {
+                return res.status(200).json(book);
+            } else {
+                return res.status(404).json({ message: "No item found!" });
+            }
+        } catch (error) {
+            reject(res.status(500).json({ mesasge: "Some Error occured in the server" }));
         }
     });
-
-    if (book != null) {
-        return res.status(200).json(book);
-    } else {
-        return res.status(404).json({ message: "No item found!" });
-    }
+    getBooks.then(() => console.log('Callback function'));
 });
 
 module.exports.general = public_users;
