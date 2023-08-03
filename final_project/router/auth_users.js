68b9154e6f3df78599388d556c53843df4c13b88
Edvvxrd <edvvxrd@gmail.com>
2023-08-03 07:28:56 -0400

    Task 9 Done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -22,13 +22,70 @@ const authenticatedUser = (username, password) => { //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
-    return res.status(200).json({ message:users })
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: username
+        }, 'access', { expiresIn: 60 * 60 });
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    let book = null;
+
+    Object.keys(books).forEach(key => {
+        if (key == isbn) {
+            book = books[key];
+        }
+    });
+
+    if (book != null) {
+        let user = req.user.data;
+        let review = req.query.review;
+        if(review) {
+            book.reviews[user] = review;
+        }
+        
+        return res.status(200).send(`The review for the book ISBN: ${isbn} has been added/updated`)
+    } else {
+        return res.status(300).json({ message: "No item found!" });
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    //Write your code here
+    const isbn = req.params.isbn;
+    let book = null;
+
+    Object.keys(books).forEach(key => {
+        if (key == isbn) {
+            book = books[key];
+        }
+    });
+
+    if (book != null) {
+        let user = req.user.data;
+        if(book.reviews[user] != undefined || book.reviews.hasProperty("user")) {
+            delete book.reviews[user];
+        }
+        return res.status(200).send(`The review for the book ISBN: ${isbn} has been deleted`)
+    } else {
+        return res.status(300).json({ message: "No item found!" });
+    }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,7 @@ public_users.post("/register", (req, res) => {
             users.push({ "username": username, "password": password });
             return res.status(200).json({ message: "User successfully registred. Now you can login" });
         } else {
-            return res.status(404).json({ message: "User already exists!" });
+            return res.status(404).json({ message: users });
         }
     }
     return res.status(404).json({ message: "Unable to register user." });
